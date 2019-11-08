375c5fd28649fd94c407a929b60d0b9909c82090
root <root@localhost.localdomain>
2019-11-08 14:23:34 -0600

    refractor TDD

--- messenger/tests.py
+++ messenger/tests.py
@@ -7,6 +7,7 @@ class ThreadTestCase(TestCase):
 	def setUp(self):
 		self.user1 = User.objects.create_user('user1',None,'test1234')
 		self.user2 = User.objects.create_user('user2',None,'test1234')
+		self.user3 = User.objects.create_user('user3',None,'test1234')
 
 		self.thread = Thread.objects.create()
 
@@ -22,3 +23,21 @@ class ThreadTestCase(TestCase):
 	def test_filter_non_existen_thread(self):
 		threads = Thread.objects.filter(users=self.user1).filter(users=self.user2)
 		self.assertEqual(len(threads),0)
+
+	def test_add_messages_to_thread(self):
+		self.thread.users.add(self.user1, self.user2)
+		message1 = Message.objects.create(user=self.user1,content="alo polesia :v")
+		message2 = Message.objects.create(user=self.user2,content="k hay")
+		self.thread.messages.add(message1, message2)
+		self.assertEqual(len(self.thread.messages.all()), 2)
+
+		for message in self.thread.messages.all():
+			print(f"{message.user}, {message.content}")
+
+	def test_add_message_from_user_not_in_thread(self):
+		self.thread.users.add(self.user1, self.user2)
+		message1 = Message.objects.create(user=self.user1,content="alo polesia :v")
+		message2 = Message.objects.create(user=self.user2,content="k hay")
+		message3 = Message.objects.create(user=self.user3,content="ola k ase v:")
+		self.thread.messages.add(message1, message2,message3)
+		self.assertEqual(len(self.thread.messages.all()),2)
